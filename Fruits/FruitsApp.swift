43fbc8fda0e2e27bfa6e3bab0bfd8f1d6dbf9095
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-06-09 16:50:37 -0400

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Lori Rothermel on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
